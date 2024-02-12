750f4aded7dfacfbb2c670ca2a64c76a129f13fc
Yu Heng Su <yolanda.su00@gmail.com>
2024-02-12 13:04:33 -0500

    Completed Task 7

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { TiDelete, TiPlus } from 'react-icons/ti';
+import { TiDelete, TiMinus, TiPlus } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -25,11 +25,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
         <td><TiPlus size='1.8em' onClick={event=> increaseAllocation(props.name)} style={{color:'white',backgroundColor:'#03C03C',borderRadius:'50%'}}>+</TiPlus></td>
+        <td><TiMinus size='1.8em' onClick={event=> decreaseAllocation(props.name)} style={{color:'white',backgroundColor:'#AA0000',borderRadius:'50%'}}>+</TiMinus></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
